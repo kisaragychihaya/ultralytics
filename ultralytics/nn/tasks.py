6aed720c931b0c1b4a8e05e3744e441988d43c71
chen- <chen-jia-shu@hotmail.com>
2024-05-28 15:54:14 +0800

    修复错误

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -885,8 +885,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             nn.ConvTranspose2d,
             DWConvTranspose2d,
             C3x,
-            RepC3,
-            EMA,RFBconv
+            RepC3
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
